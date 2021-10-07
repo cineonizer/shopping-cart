ceaf5135fbd88c9774143dfb725ca38ede016eed
cineonizer <jamesdkiam@gmail.com>
2021-10-06 20:17:08 -0700

    adding more design to the welcome screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,13 @@ import '../styles/home.scss';
 const Home = () => {
   return (
     <div className="home">
-      <img className="featured-image" src={featuredImageSource} alt="Endless featured" />
+      <div className="welcome-container">
+        <div className="welcome-message-container">
+          <div className="welcome-message-one">
+            endless. luxury.
+          </div>
+        </div>
+      </div>
       <video className="videoTag" autoPlay loop muted>
         <source src={videoSource} type="video/mp4" />
       </video>
