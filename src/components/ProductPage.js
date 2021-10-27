a582fa418c7a87f974fe9e0318b6f751dacdac63
cineonizer <jamesdkiam@gmail.com>
2021-10-26 19:49:26 -0700

    working on the product page component

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import '../styles/product.scss';
 
 const ProductPage = ({ match }) => {
   const [product, setProduct] = useState({});
@@ -8,13 +10,39 @@ const ProductPage = ({ match }) => {
         `http://localhost:3001/products/${match.params.product}`
       );
       const data = await response.json();
-      console.log(data)
+      console.log(data);
       setProduct(data);
     };
     fetchProduct();
   }, []);
 
-  return <div>hello product page</div>;
+  return (
+    <div className="product-page">
+      <div className="product-container">
+        <div className="carousel-container">
+          <Carousel
+            showArrows={true}
+            showThumbs={true}
+            showIndicators={false}
+            showStatus={false}
+          >
+            <div className="image">
+              <img src={product.image1} alt="Slide" />
+            </div>
+            <div className="image">
+              <img src={product.image2} alt="Slide" />
+            </div>
+            <div className="image">
+              <img src={product.image3} alt="Slide" />
+            </div>
+            <div className="image">
+              <img src={product.image4} alt="Slide" />
+            </div>
+          </Carousel>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
