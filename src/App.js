104497e2aa037a511ab16b7abefc6425a400c348
cineonizer <jamesdkiam@gmail.com>
2021-11-04 20:25:22 -0700

    almost finished styling the cart. need to add summary and about page

--- src/App.js
+++ src/App.js
@@ -11,7 +11,20 @@ import backgroundVideo from './assets/videos/film-burns.mp4';
 import './styles/App.scss';
 
 const App = () => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([{
+    "id": 22250442025773900,
+    "designer": "seb brown",
+    "title": "neapolitan silver, sapphire, tourmaline and emerald ring",
+    "price": "515",
+    "colour": "silver",
+    "description": "seb brown's 'neapolitan' ring is cast from recycled silver polished to a high shine but left with a characterfully brushed quality. sapphire, tourmaline and emerald stones bring a vibrant finish to its crown.",
+    "category": "fine jewelry",
+    "image1": "http://localhost:3001/assets/images/seb-brown/neopolitan-1.png",
+    "image2": "http://localhost:3001/assets/images/seb-brown/neopolitan-2.png",
+    "image3": "http://localhost:3001/assets/images/seb-brown/neopolitan-3.png",
+    "image4": "http://localhost:3001/assets/images/seb-brown/neopolitan-4.png",
+    "quantity": "1"
+}]);
 
   return (
     <Router>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,24 +4,32 @@ const Cart = ({ cart }) => {
   return (
     <div className="cart">
       <div className="cart-container">
-        <div className="items">
+        <div className="col">
+          <div className="header-title">cart</div>
+          <div className="header-qty">{cart.length} {(cart.length !== 1) ? 'items' : 'item'}</div>
           <hr />
-          <div className="header">
-            <div className="title">cart</div>
-            <div className="quantity">{cart.length} {(cart.length !== 1) ? 'items' : 'item'}</div>
-          </div>
-          <hr />
-          <div className="item">item #1</div>
-          <div className="item">item #1</div>
-          <div className="item">item #1</div>
-          <div className="item">item #1</div>
-          <div className="item">item #1</div>
+          {cart.map((product) => {
+            return (
+              <div>
+                <div className="item">
+                  <img src={product.image1} alt="" />
+                  <div className="product">
+                    <div className="designer">{product.designer}</div>
+                    <div className="title">{product.title}</div>
+                    <div className="colour">{`colour: ${product.colour}`}</div>
+                    <div className="qty">{` qty: ${product.quantity}`}</div>
+                    <div className="price">{`$${product.price}`}</div>
+                  </div>
+                </div>
+                <hr />
+              </div>
+            )
+          })}
         </div>
-        <div className="summary">summary</div>
+        <div className="summary">order summary</div>
       </div>
     </div>
   )
-
 };
 
 export default Cart;
