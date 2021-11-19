dbb15d05e5119112bb85536300395f0473519bea
cineonizer <jamesdkiam@gmail.com>
2021-11-18 19:51:59 -0800

    using json-server as backend: changed the fetch urls

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -47,7 +47,7 @@ const ProductPage = ({ match, cart, setCart }) => {
   useEffect(() => {
     const fetchProduct = async () => {
       const response = await fetch(
-        `http://localhost:3001/products/${match.params.product}`
+        `https://my-json-server.typicode.com/cineonizer/shopping-cart-data/products/${match.params.product}`
       );
       const data = await response.json();
       // convert all the json values to be lowercase to fit the aesthetic
